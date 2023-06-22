652781a368dc043c96be17fe40aea9f51537afb5
athulya116 <116333075+athulya116@users.noreply.github.com>
2023-06-22 15:18:58 +0530

    Add files via upload

---
+++ ex32.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function r(){
+
+echo "hiii"
+return 3
+
+}
+
+r
+echo $?
---
+++ ex34.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+function display(){
+echo $1
+echo $2
+echo $3
+echo $4
+}
+display athulya 21 s2mca hellooo
